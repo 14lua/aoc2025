b6274a6b1f963c67c0b9dbed9e342d81bee5d308
14lua <xhigoliluan022@gmail.com>
2025-12-03 11:38:12 +0100

    Day 2 Part 1

---
+++ day2/d2p1.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+long part_one() {
+    std::ifstream input("input.txt");
+    std::string line;
+    std::vector<std::string> ranges = {};
+    std::vector<std::string> invalid_ids = {};
+
+    std::string t;
+    while(std::getline(input, t, ',')) {
+        ranges.push_back(t);
+    }
+
+
+    for (const std::string &range : ranges) {
+        long start = std::stol(range.substr(0, range.find('-')));
+        long end = std::stol(range.substr(range.find('-') + 1));
+
+        for (long i = start; i <= end; i++) {
+            std::string id = std::to_string(i);
+
+            if (id.length() % 2 != 0) {
+                continue;
+            }
+
+            std::string id_halve_1 = id.substr(0, id.length() / 2);
+            std::string id_halve_2 = id.substr(id.length() / 2);
+
+            if (id_halve_1 == id_halve_2) {
+                invalid_ids.push_back(id);
+            }
+        }
+    }
+
+    long sum = 0;
+    for (const std::string &id : invalid_ids) {
+        sum += std::stoll(id);
+    }
+
+    return sum;
+}
+
+int main() {
+    std::cout << part_one() << std::endl;
+}
